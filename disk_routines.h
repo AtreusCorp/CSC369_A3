0df23c11df4a52c6ff38c9df82accd48a4cec8f6
Munro Keirn Yves Ross <munrokei@teach.cs.toronto.edu>
2017-07-21 19:37:44 -0400

    Adding current progress.

---
+++ disk_routines.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "ext2.h"
+#ifndef ROUTINES
+#define ROUTINES
+
+#define DIR_STR_LEN 255
+
+
+extern unsigned char *disk;
+extern struct ext2_group_desc *group_desc;
+extern struct ext2_inode *inode_table;
+
+#endif
---
+++ subroutines.c
@@ -0,0 +1,79 @@
+#include disk_routines.h
+
+// Multiplying by 2 to account for passing boot block and super block
+// put these in main
+struct ext2_group_desc *group_desc = (struct ext2_group_desc *)
+                                            (disk + 2 * EXT2_BLOCK_SIZE);
+struct ext2_inode *inode_table = (struct ext2_inode *)(disk + group_desc->bg_inode_table 
+                                                              * EXT2_BLOCK_SIZE);
+
+/* Returns a pointer to the inode in the memory mapped into by
+ * disk corresponding to inode_num.
+ */
+struct ext2_inode *fetch_inode(unsigned int inode_num){
+
+    return inode_table + (inode_table * sizeof(ext2_inode));
+}
+
+int search_dir_direct_blks(char * file_name, struct ext2_inode *dir_inode) {
+    int i;
+    char *first_entry;
+    char *cur_entry;
+
+    if (S_ISDIR(dir_inode->i_mode) != 1){
+        return -1;
+    }
+
+    for(i = 0; i < 12; ++i){
+        if ((2 ** dir_inode->i_block[i]) | group_desc->bg_block_bitmap){
+            first_entry = disk + dir_inode->i_block[i] * EXT2_BLOCK_SIZE;
+            cur_entry = first_entry;
+
+            do {
+                struct ext2_dir_entry_2 *dir_entry = ((struct ext2_dir_entry_2 *) cur_entry);
+                const char *dir_entry_name = dir_entry->name;
+                const size_t name_len = (size_t) dir_entry->name_len;
+
+                // TODO: Ask: can we still assume 1 is the bad inode?
+                if ((strncmp(file_name, dir_entry_name, name_len) == 0) && (dir_entry->inode >= 1)) {
+                    return  dir_entry->inode;
+                }
+                cur_entry += ((struct ext2_dir_entry_2 *) cur_entry)->rec_len;
+            } while ((cur_entry - first_entry) % EXT2_BLOCK_SIZE != 0);
+        } else {
+            break;
+        }
+    }
+    return -1;
+}
+
+
+/* Search the directory inode dir_inode for the file with the name of file_name.
+ * Returns the inode number of the file found, -1 if the file was not found or 
+ * dir_inode is not a directory.
+ */
+unsigned int search_dir(char *file_name, struct ext2_inode *dir_inode){
+    unsigned int inode_num_file_name;
+
+    if((inode_num_file_name = search_dir_direct_blks(file_name, dir_inode)) == -1) {
+
+        // Search remaining the indirect blocks
+        // TODO: Ask: can we still assume 1 is the bad inode?
+        
+        if (dir_inode->i_block[12] >= 1) {
+            struct ext2_inode *indirect_dir_inode = inode_table + dir_inode->i_block[12];
+        }
+    }
+}
+
+/* Given a path as a string, return the inode number of the 
+ * last file in the path.
+ */
+unsigned int get_inode_num(char *path, unsigned char *disk_ptr, unsigned int inode_num){
+	
+    char *next_slash;
+	char dir_str[DIR_STR_LEN] = path;
+	if ((next_slash = strchr(path, '/')) != NULL){
+	   strncpy(dir_str, path, next_slash - path);
+	}
+}
