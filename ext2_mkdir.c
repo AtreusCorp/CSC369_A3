527a66f2d2ed4bd4123f093e92ed1e170b062c47
Atreus <keirn.m@gmail.com>
2017-07-23 13:41:49 -0400

    Implemented a working mkdir

--- ext2_mkdir.c
+++ ext2_mkdir.c
@@ -6,9 +6,10 @@ unsigned char *disk;
 
 int main(int argc, char **argv){
 	unsigned int new_dir_inode_num;
-	unsigned int parent_dir_inode_num;
+	int parent_dir_inode_num;
 	struct ext2_inode *parent_dir_inode;
-	char target_dir[DIR_STR_LEN];
+	char target_dir[EXT2_NAME_LEN];
+    char parent_dir[EXT2_NAME_LEN];
 	char *parent_dir_end;
 	int fd = open(argv[1], O_RDWR);
 
@@ -18,21 +19,24 @@ int main(int argc, char **argv){
     }
 
     if ((new_dir_inode_num = get_inode_num(argv[2], EXT2_ROOT_INO)) > -1){
-   		printf("Error: Directory already exists.");
+   		printf("Error: Directory already exists.\n");
    		return EEXIST;
   	}
 
     // Refers to the target directory prefixed by '/'
     parent_dir_end = strrchr(argv[2], '/');
-    strncpy(target_dir, parent_dir_end + 1, DIR_STR_LEN);
-    *parent_dir_end = '\0';
+    strncpy(target_dir, parent_dir_end + 1, EXT2_NAME_LEN);
+    *(parent_dir_end + 1) = '\0';
+    strncpy(parent_dir, argv[2], EXT2_NAME_LEN);
 
-    if ((parent_dir_inode_num = get_inode_num(argv[2], 2)) < 0){
-    	printf("Error: %s does not exist.", argv[2]);
+    if ((parent_dir_inode_num = get_inode_num(parent_dir, 2)) < 0){
+    	printf("Error: %s does not exist.\n", parent_dir);
     	return ENOENT;
     }
 
     parent_dir_inode = fetch_inode_from_num(parent_dir_inode_num);
-    new_dir_inode_num = allocate_inode();
-
+    if (insert_dir_entry(parent_dir_inode, parent_dir_inode_num, 0, strlen(target_dir), EXT2_FT_DIR, target_dir) < 0){
+    	exit(1);
+    }
+    return 0;
 }
