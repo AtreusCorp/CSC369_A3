8e1576a78178a57d883b3d54813d9abf344c0f3a
Frank Lai <frankc201106@gmail.com>
2017-08-05 14:13:50 -0400

    Deleted dest_path_inode and dest_path_inode_num. And made a lot of style changes.

--- ext2_cp.c
+++ ext2_cp.c
@@ -14,8 +14,6 @@ char src_filename[EXT2_NAME_LEN + 1];
 
 // destination related names
 char dest_path[EXT2_NAME_LEN + 1];
-int dest_path_inode_num;
-struct ext2_inode *dest_path_inode;
 char dest_path_cpy[EXT2_NAME_LEN + 1];
 char dest_pdir_pathname[EXT2_NAME_LEN + 1];
 char dest_filename[EXT2_NAME_LEN + 1];
@@ -52,6 +50,7 @@ void set_filestream(char *file_path){
     fstat((fd=open(file_path, O_RDONLY)), &src_file_stat);
     if (src_file_stat.st_size > 2*12*EXT2_BLOCK_SIZE){
         printf("Error: %s's size is larger than 13 blocks", file_path);
+        close(fd);
         exit(-1);
     }
     close(fd);
@@ -103,22 +102,21 @@ int main(int argc, char **argv){
     // map the maximum file size. This utility can handle and check if original file on host exist
     set_filestream(argv[src_path_index]);
 
-    // --- BEGIN Error checking ---
-
-    // assignments for names related to dest and src
     strncpy(dest_path, argv[dest_path_index], EXT2_NAME_LEN);
     strncpy(src_path, argv[src_path_index], EXT2_NAME_LEN);
     dest_path_len = strnlen(dest_path, EXT2_NAME_LEN) + 1;
-    dest_path_inode_num = get_inode_num(dest_path, EXT2_ROOT_INO);
-    dest_path_inode = fetch_inode_from_num((unsigned) dest_path_inode_num);
 
-    if (dest_path_inode_num < 0 && dest_path[dest_path_len - 2] == '/') {
+
+    // === BEGIN Error checking ===
+
+    // Check for incorrect arguments
+    if (get_inode_num(dest_path, EXT2_ROOT_INO) < 0 && dest_path[dest_path_len - 2] == '/') {
         // dest_path is a directory. It follows that we should insert to dest_path, but it dne.
         printf("%s: The destination directory doesn't exist.\n", dest_path);
         return ENOENT;
 
-    } else if(dest_path_inode_num >= 1 && dest_path[dest_path_len - 2] != '/') {
-        // dest_path is a file. It follows that we should insert dest_filename to dest_pdir_pathname.
+    } else if(get_inode_num(dest_path, EXT2_ROOT_INO) >= 1 && dest_path[dest_path_len - 2] != '/') {
+        // dest_path is a file. It follows that dest_path is the final full destination name.
         // But file already exists
         printf("%s: The destination file already exist.\n", dest_path);
         return ENOENT;
@@ -131,17 +129,17 @@ int main(int argc, char **argv){
         exit(-1);
     }
 
-    // --- END Error checking ---
+    // === END Error checking ===
 
 
-    // --- BEGIN Names assignment ---
+    // === BEGIN dest_file and dest_pdir_pathname assignment ===
 
     // To insert a name into destination directory,
     // we need destination file name, referenced by dest_filename
     // we need destination directory's full path, referenced by dest_pdir_pathname
     if (dest_path[dest_path_len - 2] != '/') {
+        // The case dest_path is the final destination name
         strncpy(dest_path_cpy, dest_path, dest_path_len);
-        // assignments for names related to dest
         if ((dest_pdir_end = strrchr(dest_path_cpy, '/')) == NULL) {
             printf("Error: Processing destination file name");
             exit(-1);
@@ -151,6 +149,7 @@ int main(int argc, char **argv){
         strncpy(dest_pdir_pathname, dest_path_cpy, EXT2_NAME_LEN);
 
     } else {
+        // The case dest_pdir_pathname/src_filename is the final destination name
         strncpy(dest_pdir_pathname, dest_path, dest_path_len);
         // assignment for dest_filename
         if ((src_dir_end = strrchr(src_path, '/')) == NULL){
@@ -161,27 +160,29 @@ int main(int argc, char **argv){
         strncpy(dest_filename, src_filename, EXT2_NAME_LEN);
     }
 
-    // --- END names assignment ---
+    // === END dest_file and dest_pdir_pathname assignment ===
 
 
-    // BEGIN Error checking after names assignment
+    // === BEGIN Error checking after dest_file and dest_pdir_pathname are resolved ===
 
     // Check if the full destination path exists
     char dest_fullpath[EXT2_NAME_LEN];
     strncat(dest_fullpath, dest_pdir_pathname, EXT2_NAME_LEN);
     strncat(dest_fullpath, dest_filename, EXT2_NAME_LEN);
-    int dest_fullpath_inode_num = get_inode_num(dest_fullpath, EXT2_ROOT_INO);
-    if(dest_fullpath_inode_num >= 1) {
+    if(get_inode_num(dest_fullpath, EXT2_ROOT_INO) >= 1) {
         printf("%s: The destination file already exist.\n", dest_path);
         return ENOENT;
     }
 
-    // END Error checking after names assignment
+    // === END Error checking after dest_file and dest_pdir_pathname are resolved ===
+
+
+    // Directory to perform insert on
+    int dest_pdir_inode_num = get_inode_num(dest_pdir_pathname, EXT2_ROOT_INO);
+    struct ext2_inode *dest_pdir_inode = fetch_inode_from_num(dest_pdir_inode_num);
 
 
     // insert dest_filename as dir entry into dest_pdir_pathname
-    int dest_pdir_inode_num = get_inode_num(dest_pdir_pathname, EXT2_ROOT_INO);
-    struct ext2_inode *dest_pdir_inode = fetch_inode_from_num(dest_pdir_inode_num);
     if(insert_dir_entry(dest_pdir_inode,
                         dest_pdir_inode_num,
                         0,
@@ -192,23 +193,23 @@ int main(int argc, char **argv){
         exit(-1);
     }
 
-    // BEGIN copying data
+    // === BEGIN copying data ===
 
-    size_t bytes_copied = copy_stream_to_inode(src_file_stream, dest_path_inode);
+    size_t bytes_copied = copy_stream_to_inode(src_file_stream, dest_pdir_inode);
     if (bytes_copied < src_file_stat.st_size) {
         // Need 1 level of indirection here
-        dest_path_inode->i_block[12] = (unsigned) allocate_inode();
-        struct ext2_inode *indrect_inode = fetch_inode_from_num(dest_path_inode->i_block[12]);
+        dest_pdir_inode->i_block[12] = (unsigned) allocate_inode();
+        struct ext2_inode *indrect_inode = fetch_inode_from_num(dest_pdir_inode->i_block[12]);
         bytes_copied += copy_stream_to_inode(src_file_stream, indrect_inode);
         if (bytes_copied != src_file_stat.st_size) {
             printf("Bytes read does not match with file size");
             exit(-1);
         }
-        dest_path_inode->i_size += indrect_inode->i_size;
-        dest_path_inode->i_blocks += indrect_inode->i_blocks;
+        dest_pdir_inode->i_size += indrect_inode->i_size;
+        dest_pdir_inode->i_blocks += indrect_inode->i_blocks;
     }
 
-    // END copying data
+    // === END copying data ===
 
 
     return 0;
